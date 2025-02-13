ffaf816846d9ee9856914c36ab8f3f37e44db195
vitaly.morgunov <vitaly.morgunov@wn.media>
2025-02-13 14:59:15 +0400

    Initial commit

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Запуск приложения в докере
+docker compose up -d
+
+# Установка зависимостей Composer
+docker exec -w /var/www/yii-project yii_php composer install
+
+# Запуск миграций
+docker exec -w /var/www/yii-project yii_php php yii migrate --interactive=0
+
+# Создаем админа
+docker exec -w /var/www/yii-project yii_php php yii local/add-admin-user
+
